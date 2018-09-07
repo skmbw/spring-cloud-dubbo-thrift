f3fae674b296a3cae53341a786d440539930d534
skmbw <tongku2008@126.com>
2018-09-07 17:49:55 +0800

    实现thrift的service方法,mybatis config

--- xue-service/src/main/java/com/xueershangda/xueservice/XueServiceApplication.java
+++ xue-service/src/main/java/com/xueershangda/xueservice/XueServiceApplication.java
@@ -4,9 +4,10 @@ import com.vteba.tx.dao.spi.BasicDao;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
 @MapperScan(value = "com.xueershangda", markerInterface = BasicDao.class)
-@SpringBootApplication
+@SpringBootApplication(exclude = {RedisAutoConfiguration.class})
 public class XueServiceApplication {
 
 	public static void main(String[] args) {
